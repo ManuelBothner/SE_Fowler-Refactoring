faeab5062afd78e7fa44ebc4073eabb99f415c88
Manuel Bothner <renhtob@gmail.com>
2016-04-29 14:03:15 +0200

    Added Junit Test for the Movie class!

---
+++ src/MovieTest.java
@@ -0,0 +1,47 @@
+//### IMPORTS ##############################################################################################################################
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MovieTest {
+	
+	private static String title = "Ocean’s Eleven";
+	private static int priceCode = 10;
+	
+	private static Movie movie;
+	
+	@Before
+	public void clearCalculator() {
+		MovieTest.movie = new Movie(MovieTest.title, MovieTest.priceCode);
+	}
+	
+	@Test
+	public void testConstructor() {
+		
+		//Test if title is correct:
+		assertEquals(MovieTest.movie.getTitle(), MovieTest.title);
+		//Test if priceCode is correct:
+		assertEquals(MovieTest.movie.getPriceCode(), MovieTest.priceCode);
+	}
+	
+	@Test
+	public void testSetter() {
+		
+		int newPriceCode = 20;
+		
+		//Test if priceCode is no equal the new priceCode:
+		assertEquals(MovieTest.movie.getPriceCode(), MovieTest.priceCode);
+		
+		//Change the priceCode:
+		MovieTest.movie.setPriceCode(newPriceCode);
+
+		//Test if priceCode is correct:
+		assertEquals(MovieTest.movie.getPriceCode(), newPriceCode);
+		
+	}
+
+}
+
+//### EOF ##################################################################################################################################
